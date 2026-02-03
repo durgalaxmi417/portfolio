30f69f38709a7a29cfefef2509713b4a67de78a8
Durgalakshmivulcan <durgalakshmi@vulcanflow.com>
2026-02-03 17:03:03 +0530

    build2

--- src/AnimatedLine.jsx
+++ src/AnimatedLine.jsx
@@ -32,12 +32,12 @@ const ScrollRunner = () => {
             <motion.div
               key="ball"
               className="absolute -top-4 w-4 h-4 bg-white rounded-full shadow-md"
-              animate={{ x: positionX }}
               initial={{ opacity: 0 }}
               animate={{ opacity: 1, x: positionX }}
               exit={{ opacity: 0 }}
               transition={{ duration: 1.4 }}
             />
+
           )}
         </AnimatePresence>
 
